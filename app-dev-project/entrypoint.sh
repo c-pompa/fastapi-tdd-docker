48393e46b10a779fa70eb70fb2f01862218ab27c
Christian Pompa <pompa.chris@gmail.com>
2023-01-01 17:44:42 -0800

    entrypoint

--- app-dev-project/entrypoint.sh
+++ app-dev-project/entrypoint.sh
@@ -1,12 +1,4 @@
-
-##########################################################################################
-##  Summary:
-## Since the web service is dependent not only on the container being up and running 
-## but also the actual Postgres instance being up and healthy, let's add an entrypoint.
-## We referenced the Postgres container using the name of the service, web-db. 
-## The loop continues until something like Connection to web-db port 5432 [tcp/postgresql] 
-## succeeded! is returned.
-##########################################################################################
+#!/bin/sh
 
 echo "Waiting for postgres..."
 
